7ef3d1ede8d064a69782200936ceae983a3b4c4d
Momir Stanisic <Momir.stanisic.ns@hotmail.com>
2021-04-07 12:32:08 +0200

    Beta 1

--- app/src/main/java/rs/moma/spotifyshuffle/songs/SongTouchHelperCallback.kt
+++ app/src/main/java/rs/moma/spotifyshuffle/songs/SongTouchHelperCallback.kt
@@ -3,13 +3,12 @@ package rs.moma.spotifyshuffle.songs
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
-class SongTouchHelperCallback(adapter: SongAdapter) : ItemTouchHelper.Callback() {
-    private val itemAdapter = adapter
+class SongTouchHelperCallback(private val adapter: SongAdapter) : ItemTouchHelper.Callback() {
     override fun isLongPressDragEnabled(): Boolean = false
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {}
     override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int = makeMovementFlags(ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0)
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
-        itemAdapter.swapSongs(viewHolder.adapterPosition, target.adapterPosition)
+        adapter.swapSongs(viewHolder.adapterPosition, target.adapterPosition)
         return true
     }
 }
